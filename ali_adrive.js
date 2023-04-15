563e1e11b9477624f935a82454752f3510516c83
dingiangyi <73939206+dingiangyi@users.noreply.github.com>
2023-04-16 00:03:59 +0800

    Update ali_adrive.js

--- ali_adrive.js
+++ ali_adrive.js
@@ -1,3 +1,16 @@
+/******************************
+
+[rewrite_local]
+
+^https:\/\/api\.aliyundrive\.com\/apps\/v1\/users\/home\/widgets$ url script-response-body https://raw.githubusercontent.com/dingiangyi/jiejieshushu/main/ali_adrive.js
+
+[mitm] 
+
+hostname = api.aliyundrive.com
+
+
+
+*******************************/
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
