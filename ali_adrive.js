96ae52c55ae8785a08d5d9f7da1a56436aea3e3c
dingiangyi <73939206+dingiangyi@users.noreply.github.com>
2023-04-15 23:59:54 +0800

    Add files via upload

---
+++ ali_adrive.js
@@ -0,0 +1,17 @@
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+const item = [
+  "recentUsed", // 最近在看
+  // "coreFeatures", // 顶部图标
+  "activities", // 精选活动
+  "myBackup", // 我的备份
+  "recentSaved", // 最近转存
+  // "signIn" // 顶部签到
+];
+
+item.forEach((i) => {
+  delete obj[i];
+});
+
+$done({ body: JSON.stringify(obj) });
